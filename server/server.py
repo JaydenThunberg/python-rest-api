23f2818a83799a32d2f534bf378a7d6c2524c0a3
ben straumann <benstraum@gmail.com>
2020-07-16 11:42:40 -0500

    make sure to install psycopg2

--- server/server.py
+++ server/server.py
@@ -1,37 +1,31 @@
-import psycopg2
-# psycopg2: handles queries between Flask server and postgresql
-import psycopg2.extras
-# add on for psycopg2 to format postgres response as dictionary
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+import sys
+sys.path.insert(0, '/server/venv/lib/Python-3.8/site-packages')
+import psycopg2
 
-# configures connection to database
-# mainConnection = psycopg2.connect(user="nailahjohnson",
-#                                        host="localhost",
-#                                        port="5432",
-#                                        database="pets")
-
+app = Flask(__name__)                                  
 
 @app.route('/pets', methods=['GET'])
 def getPet():
     try:
-        connection = mainConnection
+           # connect to database
+        connection = psycopg2.connect(
+                                        host="localhost",
+                                        port="5432",
+                                        database="pets"
+                                        )
+        cursor = connection.cursor()  # create cursor to interact with database
 
-        # cursor: allows Python code to execute postgresql commands in database session created by connection.cursor
-        # cursor is configured to provide rows stored as dictionary
-        cursor = connection.cursor(
-            cursor_factory=psycopg2.extras.RealDictCursor)
-        sql_select_query = """ SELECT "owners"."id" AS "owner_id", "owners"."first_name", "pets"."id" as "pet_id", "pets"."breed", "pets"."color", "pets"."pet_name", "pets"."status"
-        FROM "owners" JOIN "pets" ON "owners"."id" = "pets"."owner_id" ORDER BY "pets"."status" ASC; """
+        sql_select_query = "SELECT owners.*, pets.* FROM owners JOIN pets ON owners.id = pets.owner_id ORDER BY owners.id ASC;"
         # sends query using session
         cursor.execute(sql_select_query)
         # cursor method determines response rows
         # .fetchall gets all rows satisfiying query
-        record = cursor.fetchall()
-        print(record)
+        allPets = cursor.fetchall()
+        print(allPets)
         # jsonify the returned rows
-        return jsonify(record)
+        return jsonify(allPets)
 
     except (Exception, psycopg2.Error) as error:
         # handles errors
@@ -41,15 +35,9 @@ def getPet():
     finally:
         # closing database connection.
         if(connection):
-            cursor.close()
-            # connection.close()
-            print("PostgreSQL cursor is closed")
+             cursor.close() # closes cursor
+             connection.close() 
+             print("PostgreSQL connection is closed") # logs that connection is closed
+# END GET ROUTE for PETS TABLE
 
 
-# @app.route('/pets', methods=['GET'])
-# def getPet():
-#     return{
-#         'userId': 1,
-#         'title': 'Flask React App',
-#         'completed': False
-#     }
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  
   const [initialPetData, setInitialPetData] = useState({})
 
   useEffect(()=>{
